24a9abfe53495dba6ec1fa6af1a7d113b15cc1d0
Cody Baker <cbaker@digitalocean.com>
2022-02-15 08:52:13 -0500

    remove default package vars

--- packages/load/wait/main.go
+++ packages/load/wait/main.go
@@ -12,14 +12,14 @@ func Main(args map[string]interface{}) map[string]interface{} {
 		wait, err = time.ParseDuration(waitString)
 		if err != nil {
 			return map[string]interface{}{
-				"body": fmt.Sprintf("🤮 failed to parse wait parameter: %v", err),
+				"body": fmt.Sprintf("🤮 failed to parse wait parameter: %v\n", err),
 			}
 		}
 	}
-	body := "😵‍💫 no sleep"
+	body := "😵‍💫 no sleep\n"
 	if wait != 0 {
 		time.Sleep(wait)
-		body = fmt.Sprintf("🤩 slept %s", wait.String())
+		body = fmt.Sprintf("🤩 slept %s\n", wait.String())
 	}
 	return map[string]interface{}{
 		"body": body,
